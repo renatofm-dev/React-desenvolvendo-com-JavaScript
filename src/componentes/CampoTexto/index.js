1fac4f17c398b205d83d3bb7b97eb611e47d5e3e
Renato Martins <rmartins6694@gmail.com>
2022-10-25 08:57:56 -0300

    add onChange in index.js/CampoTexto

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,9 +1,17 @@
 import './CampoTexto.css'
 const CampoTexto = (props) => {
+
+    let valor = ''
+
+
+    const aoDigitado = (event) => {
+        valor =event.target.value
+        console.log(valor)
+    }
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     )
 
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -23,7 +23,7 @@ const Formulario = () => {
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card do colaborador.</h2>;
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
                 <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
                 <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="URL da imagem"/>
